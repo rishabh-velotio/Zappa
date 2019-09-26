00a06f1b4442d3c5fdba4b5a04826c6952d58cee
aneesh14 <aneesh@cybereyelabs.io>
2019-09-26 16:42:30 +0530

    excluded other packages as well

--- zappa/cli.py
+++ zappa/cli.py
@@ -1947,11 +1947,11 @@ class ZappaCLI(object):
                         'exclude', [
                                         # "boto3",
                                         # "botocore",
-                                        "dateutil",
+                                        # "dateutil",
                                         # "s3transfer",
-                                        "six.py",
-                                        "jmespath",
-                                        "concurrent"
+                                        # "six.py",
+                                        # "jmespath",
+                                        # "concurrent"
                                     ])
             else:
                 # This could be python3.6 optimized.
@@ -1959,9 +1959,9 @@ class ZappaCLI(object):
                         'exclude', [
                                         # "boto3",
                                         # "botocore",
-                                        "dateutil",
+                                        # "dateutil",
                                         # "s3transfer",
-                                        "concurrent"
+                                        # "concurrent"
                                     ])
 
             # Create a single zip that has the handler and application
